f41daa5fc8174304bc6719fc12e9cb00fa6f4fe9
Carlos Fabián <carl.fab.g@gmail.com>
2020-04-24 23:07:28 -0500

    se crearon funciones para validar que los inputs tengan value

--- js/main.js
+++ js/main.js
@@ -3,7 +3,35 @@ const compose = (...functions) => data =>
 
   const description = $('#description')
   const calories = $('#calories')
+  const carbs = $('#carbs')
   const protein = $('#protein')
 
-  description.val()
+  description.keypress(() => {
+    description.removeClass('is-invalid')
+  })
+  calories.keypress(() => {
+    calories.removeClass('is-invalid')
+  })
+  carbs.keypress(() => {
+    carbs.removeClass('is-invalid')
+  })
+  protein.keypress(() => {
+    protein.removeClass('is-invalid')
+  })
+
+  const validateInputs = () => {
+
+    description.val() ? '' : description.addClass('is-invalid')
+    calories.val() ? '' : calories.addClass('is-invalid')
+    carbs.val() ? '' : carbs.addClass('is-invalid')
+    protein.val() ? '' : protein.addClass('is-invalid')
+
+    if(description.val() && 
+    calories.val() && 
+    carbs.val() && 
+    protein.val()
+    ) {
+      console.log('ok')
+    }
+  }
 
