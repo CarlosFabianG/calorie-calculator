8859be58145c5c609b59216f89a6fcbdb7ac1d0b
Carlos Fabián <carl.fab.g@gmail.com>
2020-04-24 21:40:14 -0500

    se agregaron ids a inputs en index.html y se declararon las variables las respectivas variblaes en de esos valores en main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
+
+  const description = $('#description')
+  const calories = $('#calories')
+  const protein = $('#protein')
+
+  description.val()
+
